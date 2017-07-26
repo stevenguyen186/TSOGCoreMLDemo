f2552d7271361f145a625f1205b19d9f5b59cb1b
Van Nguyen <van.nguyen@niteco.se>
2017-07-26 09:05:47 +0700

    first commit

---
+++ TSOG CoreML/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TSOG CoreML
+//
+//  Created by Van Nguyen on 7/25/17.
+//  Copyright © 2017 TheSchoolOfGames. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
